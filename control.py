0643c6fcde4b0e21080524256ce53355e22ab18f
Tomoya Ogura <tommy8sc@gmail.com>
2017-01-09 23:16:26 +0000

    Initial Commit

---
+++ control.py
@@ -0,0 +1,21 @@
+import subprocess
+import config
+
+def turn_on(outlet_id):
+    _send_pulse(config.CODES[outlet_id][0])
+
+def turn_off(outlet_id):
+    _send_pulse(config.CODES[outlet_id][1])
+
+# Flickers outlet_id
+def flicker(outlet_id, on_duration=1):
+    import time
+    turn_on(outlet_id)
+    time.sleep(on_duration)
+    turn_off(outlet_id)
+
+# Uses codesend to send_pulse
+def _send_pulse(pulse_id):
+    args = [config.CODESEND_DIR, '-l', str(config.PULSE), str(pulse_id)]
+    subprocess.call(args)
+
